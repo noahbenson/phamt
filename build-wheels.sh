32abb8832aaa81e7e87a6b15d8cd9a2639a3cf9f
noahbenson <nben@uw.edu>
2022-03-30 21:29:18 -0700

    trying a new strategy for deployment; take 5

--- build-wheels.sh
+++ build-wheels.sh
@@ -5,24 +5,25 @@
 # The phamt directory should be mounted in /work.
 
 PLAT="$1"
+WORK=/github/home
 
 # Compile wheels
 mkdir -p /root/tmpwheels
 for PYBIN in /opt/python/*/bin
-do "${PYBIN}/pip" wheel /work --no-deps -w /root/tmpwheels
+do "${PYBIN}/pip" wheel ${WORK} --no-deps -w /root/tmpwheels
 done
 
 # Bundle external shared libraries into the wheels
-mkdir -p /work/wheels
+mkdir -p ${WORK}/wheels
 for w in /root/tmpwheels/*.whl
 do if auditwheel show "$w"
-   then auditwheel repair "$w" --plat "$PLAT" -w /work/wheels
-   else cp "$w" /work/wheels
+   then auditwheel repair "$w" --plat "$PLAT" -w ${WORK}/wheels
+   else cp "$w" ${WORK}/wheels
    fi
 done
 
 # Install packages and run the test
 for PYBIN in /opt/python/cp3*/bin
-do "${PYBIN}/pip" install phamt --no-index -f /work/wheels
+do "${PYBIN}/pip" install phamt --no-index -f ${WORK}/wheels
    (cd "$HOME"; "${PYBIN}/python" -m unittest phamt.test)
 done
